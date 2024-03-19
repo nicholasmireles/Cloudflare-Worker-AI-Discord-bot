7e1559cc1ec900c5a8537606d13a561d69f90fde
Nick Mireles <nick.mireles@fortive.com>
2024-03-18 22:45:35 -0400

    Updated linting config

--- src/reddit.js
+++ src/reddit.js
@@ -2,7 +2,7 @@
  * Reach out to the reddit API, and get the first page of results from
  * r/aww. Filter out posts without readily available images or videos,
  * and return a random result.
- * @return The url of an image or video which is cute.
+ * @return {int} The url of an image or video which is cute.
  */
 export async function getCuteUrl() {
   const response = await fetch(redditUrl, {
