beb3e764a1713fb72750406b28e26b6f5f73adc9
Nick Mireles <nick.mireles@fortive.com>
2024-03-21 01:49:18 -0400

    rescope client cuz why not

--- src/awww.js
+++ src/awww.js
@@ -1,13 +1,12 @@
 import Cloudflare from 'cloudflare';
 const _ = import('lodash/core');
 
-const client = new Cloudflare();
-
 /**
  * Choose a random image from the repository and provide its url.
  * @return {Promise<string>} The url of an image or video which is cute.
  */
 export async function getCuteUrl() {
+  const client = new Cloudflare();
   const images = await client.images.v2.list();
   const imageURL = _.sample(images)?.variants[0];
   return imageURL;
