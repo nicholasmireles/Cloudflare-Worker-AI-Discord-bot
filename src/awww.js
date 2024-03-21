dbb1b8f8cdfb0be7b60a01e0e4402cebe15a7690
Nick Mireles <nick.mireles@fortive.com>
2024-03-21 01:28:30 -0400

    Updated tests

--- src/awww.js
+++ src/awww.js
@@ -1,15 +1,14 @@
-import process from 'node:process';
 import Cloudflare from 'cloudflare';
-var _ = require('lodash/core')
+const _ = import('lodash/core');
 
 const client = new Cloudflare();
 
 /**
  * Choose a random image from the repository and provide its url.
- * @return {string} The url of an image or video which is cute.
+ * @return {Promise<string>} The url of an image or video which is cute.
  */
 export async function getCuteUrl() {
-  var images = await client.images.v2.list()
-  var imageURL = _.sample(images)?.variants[0]
+  const images = await client.images.v2.list();
+  const imageURL = _.sample(images)?.variants[0];
   return imageURL;
 }
