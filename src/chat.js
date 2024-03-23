089e6018557a618cb6a05ce21d0a332d6f674a5e
nicholasmireles <nicholasjmireles@gmail.com>
2024-03-23 17:30:42 -0400

    Added new command

---
+++ src/chat.js
@@ -0,0 +1,12 @@
+import { Ai } from "@cloudflare/ai";
+
+/**
+ * Send the provided prompt to the genAI model and return the response.
+ * @param {string} prompt The user prompt to send to the genAI model.\
+ * @returns {string} The generated response from the model.
+ */
+export async function sendChat(prompt) {
+    const ai = new Ai()
+
+
+}
