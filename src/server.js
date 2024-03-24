39c7acff0dcd906ac6775b7b0dedc46325d40ad1
Nick Mireles <nick.mireles@fortive.com>
2024-03-23 22:14:24 -0400

    missing comma

--- src/server.js
+++ src/server.js
@@ -85,7 +85,7 @@ router.post('/', async (request, env) => {
       }
       case CHAT_COMMAND.name.toLowerCase():
         const data = await request.json();
-        console.log(data)
+        console.log(data);
         const response = await sendChat(data.options[0], env.AI);
         return new JsonResponse({
           type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
